e606374ccfcdf0c9922e055e7abf7ee2a2c93d70
renaldyhidayatt <renaldyhidayatt@gmail.com>
2022-01-23 13:36:11 +0700

    Adding

---
+++ app/Models/Food.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Food extends Model
+{
+    use HasFactory;
+    protected $fillable=['name','description','price','category_id','image'];
+
+    public function category(){
+        return $this->hasOne(Category::class, 'id', 'category_id');
+    }
+}
